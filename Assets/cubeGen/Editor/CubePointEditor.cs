000f397cfe6adfe804398a912a9924f47704fc46
Adam Chacey <adam.chacey@email.sitel.org>
2016-02-03 13:59:57 -0500

    Added AssetBundles, Audio, Common Materials, and cubeGen

---
+++ Assets/cubeGen/Editor/CubePointEditor.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using System.Threading;
+using System.Collections;
+
+[CustomEditor(typeof(CubePoint))]
+class CubePointEditor : Editor
+{
+    Texture2D aTexture = (Texture2D)AssetDatabase.LoadAssetAtPath("Assets/cubeGen/Editor/cubeGen.png", typeof(Texture2D));
+
+    public override void OnInspectorGUI()
+    {
+        CubePoint temp = (CubePoint)target;
+        if (temp != null)
+        {
+            GUILayout.BeginVertical();
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(10.0f);
+
+            temp.cubePointName = EditorGUILayout.TextField("Cubemap Name:", temp.cubePointName);
+			temp.name = temp.cubePointName;
+            
+
+            if (aTexture != null)
+                GUI.DrawTexture(new Rect(60, 300, 149, 151), aTexture);
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndVertical();
+
+            temp.tag = "Cubemap";
+        }
+    }
+}
+#endif
